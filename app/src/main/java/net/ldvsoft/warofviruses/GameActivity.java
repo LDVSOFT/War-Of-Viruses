fc7a977987f2b66450d3d8ecb6c2fef2378efbfd
LDVSOFT <lapshin.dv@mail.ru>
2015-10-16 19:26:52 +0300

    Added Game Layout

--- app/src/main/java/net/ldvsoft/warofviruses/GameActivity.java
+++ app/src/main/java/net/ldvsoft/warofviruses/GameActivity.java
@@ -1,16 +1,28 @@
 package net.ldvsoft.warofviruses;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class GameActivity extends AppCompatActivity {
+    private LinearLayout boardRoot;
+    private ImageButton[][] boardButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        boardRoot = (LinearLayout) findViewById(R.id.game_board_root);
+        buildBoard();
     }
 
     @Override
@@ -33,5 +45,31 @@ public class GameActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void buildBoard() {
+        LayoutParams BOARD_BUTTON_LAYOUT_PARAMS = new LayoutParams(0, LayoutParams.MATCH_PARENT, 1);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        float logicalDensity = metrics.density;
+        int marginValue = (int) Math.ceil(logicalDensity * (-2));
+        BOARD_BUTTON_LAYOUT_PARAMS.setMargins(marginValue, marginValue, marginValue, marginValue);
+
+        if (boardRoot.getChildCount() != 0)
+            return;
+        boardButtons = new ImageButton[10][10];
+        boardRoot.setOrientation(LinearLayout.VERTICAL);
+        for (int row = 0; row != 10; row++) {
+            LinearLayout rowLayout = new LinearLayout(this);
+            rowLayout.setOrientation(LinearLayout.HORIZONTAL);
+            for (int column = 0; column != 10; column++) {
+                ImageButton newButton = new ImageButton(this);
+                boardButtons[row][column] = newButton;
+                rowLayout.addView(boardButtons[row][column], BOARD_BUTTON_LAYOUT_PARAMS);
+            }
+            boardRoot.addView(rowLayout, new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1));
+        }
+        boardRoot.invalidate();
     }
 }
