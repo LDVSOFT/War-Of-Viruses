b181ee248aae47bd25aab647da219157ed9db416
LDVSOFT <lapshin.dv@mail.ru>
2015-12-01 11:34:52 +0300

    Rewritten server to be more moduled, fixed a lot of stuff

---
+++ server/src/main/java/net/ldvsoft/warofviruses/GCMHandler.java
@@ -0,0 +1,63 @@
+package net.ldvsoft.warofviruses;
+
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.XMPPException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by ldvsoft on 24.11.15.
+ */
+public class GCMHandler extends SmackCcsClient {
+    protected Logger logger = Logger.getLogger(GCMHandler.class.getName());
+    protected WarOfVirusesServer server;
+
+    public GCMHandler(WarOfVirusesServer server) throws XMPPException, IOException, SmackException {
+        this.server = server;
+        connect(server.getSetting("google.projectId", ""), server.getSetting("google.apiKey", ""));
+    }
+
+    @Override
+    public boolean sendDownstreamMessage(String jsonRequest) {
+        try {
+            return super.sendDownstreamMessage(jsonRequest);
+        } catch (SmackException.NotConnectedException e) {
+            logger.log(Level.WARNING, "Why connection is closed?", e);
+            return false;
+        }
+    }
+
+    @Override
+    protected void handleUpstreamMessage(JSONObject message) {
+        super.handleUpstreamMessage(message);
+        JSONObject answer = server.process(message);
+        if (answer != null) {
+            sendDownstreamMessage(SmackCcsClient.createJsonMessage(
+                    message.getString("from"),
+                    nextMessageId(),
+                    answer,
+                    null,
+                    null,
+                    false,
+                    "high"
+            ));
+        }
+    }
+
+    @Override
+    protected void handleAckReceipt(JSONObject jsonObject) {
+        super.handleAckReceipt(jsonObject);
+    }
+
+    @Override
+    protected void handleNackReceipt(JSONObject jsonObject) {
+        super.handleNackReceipt(jsonObject);
+    }
+
+    public void stop() {
+        disconnect();
+    }
+}
--- server/src/main/java/net/ldvsoft/warofviruses/WarOfVirusesServer.java
+++ server/src/main/java/net/ldvsoft/warofviruses/WarOfVirusesServer.java
@@ -1,20 +1,151 @@
 package net.ldvsoft.warofviruses;
 
-public class WarOfVirusesServer {
-    public static void main(String[] args) {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import static net.ldvsoft.warofviruses.WoVProtocol.ACTION;
+import static net.ldvsoft.warofviruses.WoVProtocol.ACTION_PING;
+import static net.ldvsoft.warofviruses.WoVProtocol.ACTION_TEST;
+import static net.ldvsoft.warofviruses.WoVProtocol.PARAM_TOKEN;
+import static net.ldvsoft.warofviruses.WoVProtocol.PING_ID;
+import static net.ldvsoft.warofviruses.WoVProtocol.RESULT;
+import static net.ldvsoft.warofviruses.WoVProtocol.RESULT_FAILURE;
+import static net.ldvsoft.warofviruses.WoVProtocol.RESULT_SUCCESS;
+
+public final class WarOfVirusesServer {
+    protected static final JSONObject JSON_RESULT_FAILURE = new JSONObject().put(RESULT, RESULT_FAILURE);
+
+    private static final String DEFAULT_CONFIG_FILE = "/etc/war-of-viruses-server.conf";
+
+    private Logger logger;
+
+    private Properties config = new Properties();
+
+    private HTTPHandler httpHandler;
+    private GCMHandler gcmHandler;
+
+    public String getSetting(String name, String defaultValue) {
+        return config.getProperty(name, defaultValue);
+    }
+
+    /**
+     * Process incoming message from client.
+     * If simple answer is required, returns an answer.
+     * May return null, but then client will get generic answer.
+     * @param message Message from the client.
+     * @return (Optional) Answer to client.
+     */
+    public JSONObject process(JSONObject message) {
+        if (! message.has(ACTION))
+            return null;
         try {
-            final HTTPHandler httpHandler = new HTTPHandler(9001);
+            switch (message.getString(ACTION)) {
+                case ACTION_PING:
+                    return processPing(message);
+                case ACTION_TEST:
+                    return processTest(message);
+                default:
+                    return null;
+            }
+        } catch (JSONException e) {
+            logger.log(Level.WARNING, "Wrong message format", e);
+            return null;
+        }
+    }
+
+    private JSONObject processPing(JSONObject message) {
+        JSONObject data = message.getJSONObject("data");
+        String sender = data.getString("from");
+        String messageId = data.getString("message_id");
+
+        JSONObject answer = new JSONObject()
+                .put(RESULT, RESULT_SUCCESS)
+                .put(PING_ID, messageId);
+
+        gcmHandler.sendDownstreamMessage(SmackCcsClient.createJsonMessage(
+                        sender,
+                        gcmHandler.nextMessageId(),
+                        answer,
+                        null,
+                        null,
+                        false,
+                        "high")
+        );
+        return null;
+    }
+
+    public JSONObject processTest(JSONObject message) {
+        String token = message.getString(PARAM_TOKEN);
+
+        JSONObject gcmTestMessage = new JSONObject()
+                .put(RESULT, RESULT_SUCCESS)
+                .put("Hello", "World");
+
+        boolean sent = gcmHandler.sendDownstreamMessage(SmackCcsClient.createJsonMessage(
+                        token,
+                        gcmHandler.nextMessageId(),
+                        gcmTestMessage,
+                        "TRALALALALA",
+                        (long) 3600,
+                        false,
+                        "high")
+        );
+
+        return new JSONObject()
+                .put(RESULT, sent ? RESULT_SUCCESS : RESULT_FAILURE);
+    }
+
+    public void run() {
+        String configFile = System.getenv("CONFIG");
+        if (configFile == null) {
+            configFile = DEFAULT_CONFIG_FILE;
+        }
+
+        logger = Logger.getLogger(WarOfVirusesServer.class.getName());
+        try {
+            LogManager.getLogManager().readConfiguration(new FileInputStream(configFile));
+            config.load(new FileReader(configFile));
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Cannot open config file or it's contents are wrong.", e);
+            System.exit(1);
+        }
+        logger = Logger.getLogger(WarOfVirusesServer.class.getName());
+
+        try {
+            httpHandler = new HTTPHandler(this);
+            gcmHandler = new GCMHandler(this);
 
             Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
                 @Override
                 public void run() {
                     httpHandler.stop();
+                    gcmHandler.stop();
                 }
             }));
+
+            while (!Thread.interrupted()) {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
         } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Server failed to start!\n");
+            logger.log(Level.SEVERE, "Server failed", e);
             System.exit(1);
         }
+    }
+
+    public static void main(String[] args) {
+        WarOfVirusesServer instance = new WarOfVirusesServer();
+        instance.run();
     }
 }
