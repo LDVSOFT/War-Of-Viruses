89e20f8f2f8892e05fc2c535ceacc14973a30fa1
tehnar <tehnar5@gmail.com>
2015-12-09 13:01:41 +0300

    New serialization in progress

--- app/src/main/java/net/ldvsoft/warofviruses/GameHistoryDBOpenHelper.java
+++ app/src/main/java/net/ldvsoft/warofviruses/GameHistoryDBOpenHelper.java
@@ -18,25 +18,55 @@ import java.util.Locale;
 public class GameHistoryDBOpenHelper extends SQLiteOpenHelper {
     private static final int VERSION = 8;
     private static final String DB_NAME = "gameHistoryDB";
-    private static final String GAME_HISTORY_TABLE = "gameHistoryTable";
+    private static final String GAME_TABLE = "Game";
     private static final String ID = "id";
     private static final String GAME_DATA = "gameData";
-    private static final String IS_FINISHED = "isFinished";
+    private static final String GAME_STATUS = "status";
     private static final String GAME_DATE = "gameDate";
+    private static final String PLAYER_ZERO = "playerZero";
+    private static final String PLAYER_CROSSES = "playerCrosses";
 
-    private static final String CREATE_TABLE = "CREATE TABLE " + GAME_HISTORY_TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            GAME_DATA + " BLOB, " + IS_FINISHED + " INTEGER, " + GAME_DATE + " TEXT);";
+    private static final String TURN_TABLE = "Turn";
+    private static final String GAME = "game";
+    private static final String TURN_NUMBER = "turnNo";
+    private static final String TURN_TYPE = "type";
+    private static final String TURN_X = "x";
+    private static final String TURN_Y = "y";
 
-    private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + GAME_HISTORY_TABLE + ";";
+    private static final String CREATE_GAME_TABLE = "CREATE TABLE " + GAME_TABLE + "(" + ID + " INT PRIMARY KEY AUTOINCREMENT, " +
+            PLAYER_CROSSES + " INT UNSIGNED NOT NULL, " + PLAYER_ZERO + " INT  UNSIGNED NOT NULL, " + GAME_STATUS +
+            " INT NOT NULL, " + GAME_DATE + " TEXT);"; //todo: foreign key for player ids
 
-    private static final String GET_ACTIVE_GAME = "SELECT " + GAME_DATA + " FROM " + GAME_HISTORY_TABLE + " WHERE " + IS_FINISHED + " = 0;";
+    private static final String CREATE_TURN_TABLE = "CREATE TABLE " + TURN_TABLE + "(" + GAME + " INT UNSIGNED NOT NULL, " +
+            TURN_NUMBER + " INT UNSIGNED NOT NULL, " + TURN_TYPE + " INT NOT NULL, " + TURN_X + " INT NULL, " + TURN_Y + " INT NULL, " +
+            "PRIMARY KEY(" + GAME + ", " + TURN_NUMBER + "), FOREIGN KEY (" + GAME + ") REFERENCES " + GAME + "(" + ID + ")" + ");";
 
-    private static final String DELETE_ACTIVE_GAME = "DELETE FROM " + GAME_HISTORY_TABLE + " WHERE " + IS_FINISHED + " = 0;";
-    private static final String GET_GAME_HISTORY = "SELECT " + ID + ", " + GAME_DATE + " FROM " + GAME_HISTORY_TABLE + " WHERE " +
-            IS_FINISHED + " = 1 ORDER BY " + GAME_DATE + " DESC;";
-    private static final String GET_GAME_BY_ID = "SELECT " + GAME_DATA + " FROM " + GAME_HISTORY_TABLE + " WHERE " + ID + " = ?;";
+    private static final String GET_ACTIVE_GAME = "SELECT " + TURN_TYPE + ", " + TURN_X + ", " + TURN_Y + ", " +
+            " FROM " + TURN_TABLE + " WHERE " + GAME_STATUS + " = 0 ORDER BY " + TURN_NUMBER + " ASC;";
+
+    private static final String DELETE_ACTIVE_GAME_TURNS = "DELETE t FROM " + TURN_TABLE + "t INNER JOIN " + GAME_TABLE +
+            " ON (" + GAME_TABLE + "." + ID + " == " + TURN_TABLE + "." + GAME + " AND NOT " + GAME + "." + GAME_STATUS + ";";
+
+    private static final String DELETE_ACTIVE_GAME = "DELETE FROM " + GAME_TABLE + " WHERE " + GAME_STATUS +
+            " =" + GameStatus.RUNNING.ordinal() + ";";
+
+    private static final String GET_GAME_HISTORY = "SELECT " + ID + ", " + GAME_DATE + " FROM " + GAME_TABLE + " WHERE " +
+            GAME_STATUS + " = 1 ORDER BY " + GAME_DATE + " DESC;";
+
+    private static final String GET_GAME_BY_ID = "SELECT " + GAME_DATA + " FROM " + GAME_TABLE + " WHERE " + ID + " = ?;";
+
+    private static final String ADD_GAME = "INSERT INTO " + GAME_TABLE + "(" + ID + ", " + PLAYER_CROSSES + ", " + PLAYER_ZERO +
+            ", " + GAME_STATUS + ", " + GAME_DATE + ") VALUES (?, ?, ?, ?, ?);";
+
+    private static final String ADD_GAME_TURNS = "INSERT INTO " + TURN_TABLE + "(" + GAME + ", " + TURN_NUMBER + ", " + TURN_TYPE +
+            ", " + TURN_X + ", " + TURN_Y + ") VALUES (?, ?, ?, ?, ?);";
 
     private static GameHistoryDBOpenHelper instance;
+
+    private static final String DROP_GAME_TABLE = "DROP TABLE IF EXISTS " + GAME_TABLE + ";";
+    private static final String DROP_TURN_TABLE = "DROP TABLE IF EXISTS " + TURN_TABLE + ";";
+
+    private enum GameStatus {RUNNING, FINISHED, DELETED}; //probably not there, in some other class
 
     public synchronized static GameHistoryDBOpenHelper getInstance(Context context) {
         if (instance == null) {
@@ -51,27 +81,29 @@ public class GameHistoryDBOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE);
+        db.execSQL(CREATE_GAME_TABLE);
+        db.execSQL(CREATE_TURN_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(DROP_TABLE);
+        db.execSQL(DROP_GAME_TABLE);
+        db.execSQL(DROP_TURN_TABLE);
         onCreate(db);
     }
 
-    public void addGame(byte[] serializedGame, boolean isFinished) {
+    public void addGame(Game game) {
         ContentValues cv = new ContentValues();
         cv.put(GAME_DATA, serializedGame);
-        cv.put(IS_FINISHED, isFinished);
+        cv.put(GAME_STATUS, isFinished);
         Calendar c = Calendar.getInstance();
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
         String formattedDate = df.format(c.getTime());
         cv.put(GAME_DATE, formattedDate);
-        getWritableDatabase().insert(GAME_HISTORY_TABLE, null, cv);
+        getWritableDatabase().insert(GAME_TABLE, null, cv);
     }
 
-    public byte[] getSerializedActiveGame() {
+    public Game getSerializedActiveGame() {
         Cursor cursor = getReadableDatabase().rawQuery(GET_ACTIVE_GAME, null);
         Log.d("DBHelper", "Loading active game: found " + cursor.getCount() + " active games");
         if (!cursor.moveToFirst()) {
@@ -103,7 +135,7 @@ public class GameHistoryDBOpenHelper extends SQLiteOpenHelper {
         return history;
     }
 
-    public byte[] getGameById(int id) {
+    public Game getGameById(int id) {
         Cursor cursor = getReadableDatabase().rawQuery(GET_GAME_BY_ID, new String[] {Integer.toString(id)});
         Log.d("DBHelper", "Loading game by id: found " + cursor.getCount() + " games");
         if (!cursor.moveToFirst()) {
