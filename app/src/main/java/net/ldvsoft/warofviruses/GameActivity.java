70a310e8dc26d7f05e520a8fed1833966b9045df
tehnar <tehnar5@gmail.com>
2015-12-10 15:15:46 +0300

    Now game turns are running in separate thread

--- app/src/main/java/net/ldvsoft/warofviruses/GameActivity.java
+++ app/src/main/java/net/ldvsoft/warofviruses/GameActivity.java
@@ -74,52 +74,83 @@ public class GameActivity extends GameActivityBase {
                 .unregisterReceiver(tokenSentReceiver);
         super.onPause();
     }
+
+    private class OnSkipTurnListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... params) {
+                    if (isEnemyLocalPlayer) {
+                        if (!game.skipTurn(game.getCurrentPlayer())) {
+                            return null;
+                        }
+                    } else {
+                        if (!game.skipTurn(humanPlayer)) {
+                            return null;
+                        }
+                    }
+                    return null;
+                }
+            }.execute();
+
+        }
+    }
+
+    private class OnGiveUpListener implements  View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            new AsyncTask<Void, Void, Void>() {
+
+                @Override
+                protected Void doInBackground(Void... params) {
+                    if (isEnemyLocalPlayer) {
+                        game.giveUp(game.getCurrentPlayer());
+                    } else {
+                        game.giveUp(humanPlayer);
+                    }
+                    return null;
+                }
+            }.execute();
+        }
+    }
+
+    private class OnBoardClickListener implements View.OnClickListener {
+        private final int x, y;
+
+        OnBoardClickListener(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        @Override
+        public void onClick(View v) {
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... params) {
+                    if (isEnemyLocalPlayer) {
+                        game.doTurn(game.getCurrentPlayer(), x, y);
+                    } else {
+                        game.doTurn(humanPlayer, x, y);
+                    }
+                    return null;
+                }
+            }.execute();
+        }
+    }
+
     private void initButtons() {
         Button skipTurnButton = (Button) findViewById(R.id.game_button_passturn);
-        skipTurnButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isEnemyLocalPlayer) {
-                    if (!game.skipTurn(game.getCurrentPlayer())) {
-                        return;
-                    }
-                } else {
-                    if (!game.skipTurn(humanPlayer)) {
-                        return;
-                    }
-                }
-                redrawGame(game.getGameLogic());
-            }
-        });
-
+        skipTurnButton.setOnClickListener(new OnSkipTurnListener());
         Button giveUpButton = (Button) findViewById(R.id.game_button_giveup);
-        giveUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isEnemyLocalPlayer) {
-                    game.giveUp(game.getCurrentPlayer());
-                } else {
-                    game.giveUp(humanPlayer);
-                }
-            }
-        });
+        giveUpButton.setOnClickListener(new OnGiveUpListener());
 
         for (int i = 0; i != BOARD_SIZE; i++)
             for (int j = 0; j != BOARD_SIZE; j++) {
-                final int x = i;
-                final int y = j;
-                boardButtons[i][j].setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (isEnemyLocalPlayer) {
-                            game.doTurn(game.getCurrentPlayer(), x, y);
-                        } else {
-                            game.doTurn(humanPlayer, x, y);
-                        }
-                    }
-                });
+                boardButtons[i][j].setOnClickListener(new OnBoardClickListener(i, j));
             }
     }
+
     @Override
     protected void onStop() {
         Log.d("GameActivityBase", "onStop");
@@ -138,9 +169,15 @@ public class GameActivity extends GameActivityBase {
         game.setOnGameStateChangedListener(new Game.OnGameStateChangedListener() {
             @Override
             public void onGameStateChanged() {
-                redrawGame(game.getGameLogic());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        redrawGame(game.getGameLogic());
+                    }
+                });
             }
         });
+
         game.setOnGameFinishedListener(new Game.OnGameFinishedListener() {
             @Override
             public void onGameFinished() {
