0b754ffa745e3c93d9cc2c8a73aca423a073726b
tehnar <tehnar5@gmail.com>
2015-12-13 15:37:12 +0300

    Added userType field and getters

--- logic/src/main/java/net/ldvsoft/warofviruses/User.java
+++ logic/src/main/java/net/ldvsoft/warofviruses/User.java
@@ -6,20 +6,24 @@ package net.ldvsoft.warofviruses;
 public class User {
     private long id;
     private String googleToken;
+    private int type;
     private String nickNameStr;
     private String nickNameId;
     private int colorCross;
     private int colorZero;
     private User invitationTarget;
+    private byte[] nicknameStr;
 
     public User(
             long id,
             String googleToken,
+            int type,
             String nickNameStr, String nickNameId,
             int colorCross, int colorZero,
             User invitationTarget) {
         this.id = id;
         this.googleToken = googleToken;
+        this.type = type;
         this.nickNameStr = nickNameStr;
         this.nickNameId = nickNameId;
         this.colorCross = colorCross;
@@ -33,5 +37,29 @@ public class User {
 
     public long getId() {
         return id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getGoogleToken() {
+        return googleToken;
+    }
+
+    public String getNickNameStr() {
+        return nickNameStr;
+    }
+
+    public String getNickNameId() {
+        return nickNameId;
+    }
+
+    public int getColorCross() {
+        return colorCross;
+    }
+
+    public int getColorZero() {
+        return colorZero;
     }
 }
