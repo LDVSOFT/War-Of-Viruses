71058144d4ac3b939178959913d204358ec868e4
tehnar <tehnar5@gmail.com>
2015-12-14 15:07:32 +0300

    Sending turn information as JSON string

--- app/src/main/java/net/ldvsoft/warofviruses/ClientNetworkPlayer.java
+++ app/src/main/java/net/ldvsoft/warofviruses/ClientNetworkPlayer.java
@@ -58,11 +58,13 @@ public class ClientNetworkPlayer extends Player {
     @Override
     public void onGameStateChanged(GameEvent event) {
         Bundle data = new Bundle();
+        JsonObject jsonData = new JsonObject();
         data.putString(WoVProtocol.ACTION, WoVProtocol.ACTION_TURN);
         String id = UUID.randomUUID().toString();
-        data.putInt(WoVProtocol.TURN_TYPE, event.getEventTypeAsInt());
-        data.putInt(WoVProtocol.TURN_X, event.getTurnX());
-        data.putInt(WoVProtocol.TURN_Y, event.getTurnY());
+        jsonData.addProperty(WoVProtocol.TURN_TYPE, event.getEventTypeAsInt());
+        jsonData.addProperty(WoVProtocol.TURN_X, event.getTurnX());
+        jsonData.addProperty(WoVProtocol.TURN_Y, event.getTurnY());
+        data.putString(WoVProtocol.DATA, jsonData.toString());
         try {
             gcm.send(context.getString(R.string.gcm_defaultSenderId) + "@gcm.googleapis.com", id, data);
         } catch (IOException e) {
