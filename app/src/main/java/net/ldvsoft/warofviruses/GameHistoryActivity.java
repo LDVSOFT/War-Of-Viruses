ed667f344ad3a637f83ddef1508c00fce901d259
LDVSOFT <lapshin.dv@mail.ru>
2015-12-16 21:29:51 +0300

    Now History is really clickable and viewable!

--- app/src/main/java/net/ldvsoft/warofviruses/GameHistoryActivity.java
+++ app/src/main/java/net/ldvsoft/warofviruses/GameHistoryActivity.java
@@ -55,7 +55,7 @@ public class GameHistoryActivity extends AppCompatActivity {
         RecyclerView recycler = (RecyclerView) findViewById(R.id.games);
         recycler.setHasFixedSize(true);
         recycler.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new MyAdapter(this);
+        adapter = new MyAdapter();
         recycler.setAdapter(adapter);
 
         adapter.notifyDataSetChanged();
@@ -102,111 +102,114 @@ public class GameHistoryActivity extends AppCompatActivity {
                 return false;
         }
     }
+
+    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private long userId;
+        private ArrayList<Game> data;
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(GameHistoryActivity.this).inflate(R.layout.row_game_relpay, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            final Game game = data.get(position);
+            switch (game.getMineFigure(userId)) {
+                case NONE:
+                    holder.opponent.setText(getString(R.string.GAME_LOCAL));
+                    holder.figure.setImageDrawable(BoardCellButton.cellEmpty);
+                    switch (game.getGameState()) {
+                        case CROSS_WON:
+                            holder.result.setText(getString(R.string.GAME_CROSS_WON));
+                            break;
+                        case ZERO_WON:
+                            holder.result.setText(getString(R.string.GAME_ZERO_WON));
+                            break;
+                        case DRAW:
+                            holder.result.setText(getString(R.string.GAME_DRAW));
+                            break;
+                    }
+                    break;
+                case CROSS:
+                    holder.opponent.setText(game.getZeroPlayer().getName());
+                    switch (game.getGameState()) {
+                        case CROSS_WON:
+                            holder.figure.setImageDrawable(BoardCellButton.cellCross);
+                            holder.result.setText(getString(R.string.GAME_WON));
+                            break;
+                        case ZERO_WON:
+                            holder.figure.setImageDrawable(BoardCellButton.cellCrossDead);
+                            holder.result.setText(getString(R.string.GAME_LOST));
+                            break;
+                        case DRAW:
+                            holder.figure.setImageDrawable(BoardCellButton.cellEmpty);
+                            holder.result.setText(getString(R.string.GAME_DRAW));
+                    }
+                    break;
+                case ZERO:
+                    holder.opponent.setText(game.getCrossPlayer().getName());
+                    switch (game.getGameState()) {
+                        case CROSS_WON:
+                            holder.figure.setImageDrawable(BoardCellButton.cellZeroDead);
+                            holder.result.setText(getString(R.string.GAME_LOST));
+                            break;
+                        case ZERO_WON:
+                            holder.figure.setImageDrawable(BoardCellButton.cellZero);
+                            holder.result.setText(getString(R.string.GAME_WON));
+                            break;
+                        case DRAW:
+                            holder.figure.setImageDrawable(BoardCellButton.cellEmpty);
+                            holder.result.setText(getString(R.string.GAME_DRAW));
+                    }
+                    break;
+            }
+            Date date = new GregorianCalendar(2016, 9, 1, 15, 35).getTime();
+            holder.date.setText(DateFormat.getDateFormat(GameHistoryActivity.this).format(date));
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(GameHistoryActivity.this, GameActivityReplay.class);
+                    intent.putExtra(WoVPreferences.REPLAY_GAME_ID, game.getGameId());
+                    startActivity(intent);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            if (data == null) {
+                return 0;
+            }
+            return data.size();
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return data.get(position).getGameId();
+        }
+
+        public void fetchData(long userId, ArrayList<Game> data) {
+            this.userId = userId;
+            this.data = data;
+            notifyDataSetChanged();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            private BoardCellButton figure;
+            private TextView opponent;
+            private TextView result;
+            private TextView date;
+
+            private ViewHolder(View view) {
+                super(view);
+                figure = (BoardCellButton) view.findViewById(R.id.figure);
+                opponent = (TextView) view.findViewById(R.id.opponent);
+                result = (TextView) view.findViewById(R.id.result);
+                date = (TextView) view.findViewById(R.id.date);
+            }
+        }
+    }
+
 }
-
-class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-    private long userId;
-    private ArrayList<Game> data;
-    private Activity context;
-
-    public MyAdapter(Activity context) {
-        this.context = context;
-    }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.row_game_relpay, parent, false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Game game = data.get(position);
-        switch (game.getMineFigure(userId)) {
-            case NONE:
-                holder.opponent.setText(context.getString(R.string.GAME_LOCAL));
-                holder.figure.setImageDrawable(BoardCellButton.cellEmpty);
-                switch (game.getGameState()) {
-                    case CROSS_WON:
-                        holder.result.setText(context.getString(R.string.GAME_CROSS_WON));
-                        break;
-                    case ZERO_WON:
-                        holder.result.setText(context.getString(R.string.GAME_ZERO_WON));
-                        break;
-                    case DRAW:
-                        holder.result.setText(context.getString(R.string.GAME_DRAW));
-                        break;
-                }
-                break;
-            case CROSS:
-                holder.opponent.setText(game.getZeroPlayer().getName());
-                switch (game.getGameState()) {
-                    case CROSS_WON:
-                        holder.figure.setImageDrawable(BoardCellButton.cellCross);
-                        holder.result.setText(context.getString(R.string.GAME_WON));
-                        break;
-                    case ZERO_WON:
-                        holder.figure.setImageDrawable(BoardCellButton.cellCrossDead);
-                        holder.result.setText(context.getString(R.string.GAME_LOST));
-                        break;
-                    case DRAW:
-                        holder.figure.setImageDrawable(BoardCellButton.cellEmpty);
-                        holder.result.setText(context.getString(R.string.GAME_DRAW));
-                }
-                break;
-            case ZERO:
-                holder.opponent.setText(game.getCrossPlayer().getName());
-                switch (game.getGameState()) {
-                    case CROSS_WON:
-                        holder.figure.setImageDrawable(BoardCellButton.cellZeroDead);
-                        holder.result.setText(context.getString(R.string.GAME_LOST));
-                        break;
-                    case ZERO_WON:
-                        holder.figure.setImageDrawable(BoardCellButton.cellZero);
-                        holder.result.setText(context.getString(R.string.GAME_WON));
-                        break;
-                    case DRAW:
-                        holder.figure.setImageDrawable(BoardCellButton.cellEmpty);
-                        holder.result.setText(context.getString(R.string.GAME_DRAW));
-                }
-                break;
-        }
-        Date date = new GregorianCalendar(2016, 9, 1, 15, 35).getTime();
-        holder.date.setText(DateFormat.getDateFormat(context).format(date));
-    }
-
-    @Override
-    public int getItemCount() {
-        if (data == null) {
-            return 0;
-        }
-        return data.size();
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return data.get(position).getGameId();
-    }
-
-    public void fetchData(long userId, ArrayList<Game> data) {
-        this.userId = userId;
-        this.data = data;
-        notifyDataSetChanged();
-    }
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        private BoardCellButton figure;
-        private TextView opponent;
-        private TextView result;
-        private TextView date;
-
-        private ViewHolder(View view) {
-            super(view);
-            figure = (BoardCellButton) view.findViewById(R.id.figure);
-            opponent = (TextView) view.findViewById(R.id.opponent);
-            result = (TextView) view.findViewById(R.id.result);
-            date = (TextView) view.findViewById(R.id.date);
-        }
-    }
-}
-
