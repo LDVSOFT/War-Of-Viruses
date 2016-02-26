2e545f09227de7696da5183aa856b5dd51201688
LDVSOFT <lapshin.dv@mail.ru>
2016-02-26 15:37:59 +0300

    (WIP) Bringing back Google accounts registration

--- app/src/main/java/net/ldvsoft/warofviruses/MenuActivity.java
+++ app/src/main/java/net/ldvsoft/warofviruses/MenuActivity.java
@@ -1,10 +1,8 @@
 package net.ldvsoft.warofviruses;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
@@ -22,13 +20,22 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.gcm.GoogleCloudMessaging;
-
-import java.io.IOException;
-import java.util.UUID;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.gson.JsonObject;
 
 public class MenuActivity extends AppCompatActivity {
-    private GameLoadedFromServerReceiver gameLoadedFromServerReceiver = null;
+    private static final int RC_SIGN_IN = 9000;
+    private static final IntentFilter INTENT = new IntentFilter(WoVPreferences.MAIN_BROADCAST);
+
+    private GcmMessagesReceiver gcmMessagesReceiver = null;
     private TextView userName;
     private BoardCellButton crossButton;
     private BoardCellButton zeroButton;
@@ -40,6 +47,9 @@ public class MenuActivity extends AppCompatActivity {
     private Button restoreGameButton;
     /*FIXME*/
     private FigureSet figureSet = new FigureSet();
+
+    private GoogleApiClient apiClient;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +83,13 @@ public class MenuActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
+                    case R.id.drawer_login:
+                        logIn();
+                        drawerLayout.closeDrawers();
+                        return true;
+                    case R.id.drawer_logout:
+                        logOut();
+                        drawerLayout.closeDrawers();
                     case R.id.drawer_clear_db:
                         clearDB();
                         return true;
@@ -87,8 +104,64 @@ public class MenuActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        apiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult connectionResult) {
+                        Toast.makeText(MenuActivity.this, "No Google?", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        gcmMessagesReceiver = new GcmMessagesReceiver();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(apiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            showProgressDialog(getString(R.string.menu_logging_back));
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    hideProgressDialog();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+
+        registerReceiver(gcmMessagesReceiver, INTENT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
 
     @Override
     protected void onResume() {
@@ -98,10 +171,7 @@ public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onStop() {
-        if (gameLoadedFromServerReceiver != null) {
-            unregisterReceiver(gameLoadedFromServerReceiver);
-            gameLoadedFromServerReceiver = null;
-        }
+        unregisterReceiver(gcmMessagesReceiver);
         super.onStop();
     }
 
@@ -116,73 +186,16 @@ public class MenuActivity extends AppCompatActivity {
         }
     }
 
-    private class RestoreGameDialog {
-        private final Runnable loadGame;
-
-        RestoreGameDialog(Runnable loadGame) {
-            this.loadGame = loadGame;
-        }
-
-        public void execute() {
-            new AlertDialog.Builder(MenuActivity.this)
-                    .setMessage("Found saved game. What should I do with it?") //todo: more understandable options
-                    .setCancelable(false)
-                    .setPositiveButton("Load it", new RestoreGame())
-                    .setNeutralButton("Do nothing", null)
-                    .setNegativeButton("Give up and start new game", new NewGame())
-                    .show();
-        }
-
-        private class RestoreGame implements Dialog.OnClickListener {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                restoreSavedGame(null);
-            }
-        }
-
-        private class NewGame implements Dialog.OnClickListener {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Game game = DBOpenHelper.getInstance(MenuActivity.this).getAndRemoveActiveGame();
-                game.giveUp(game.getCurrentPlayer()); //todo: give up for me, not for current player!
-                DBOpenHelper.getInstance(MenuActivity.this).addGame(game);
-                loadGame.run();
-            }
-        }
-    }
-
-    private class PlayAgainstBot implements Runnable {
-        @Override
-        public void run() {
-            Intent intent = new Intent(MenuActivity.this, GameActivity.class);
-            intent.putExtra(WoVPreferences.OPPONENT_TYPE, WoVPreferences.OPPONENT_BOT);
-            startActivity(intent);
-        }
-    }
-
     public void playAgainstBot(View view) {
-        if (DBOpenHelper.getInstance(this).hasActiveGame()) {
-            new RestoreGameDialog(new PlayAgainstBot()).execute();
-            return;
-        }
-        new PlayAgainstBot().run();
-    }
-
-    private class PlayAgainstLocalPlayer implements Runnable {
-        @Override
-        public void run() {
-            Intent intent = new Intent(MenuActivity.this, GameActivity.class);
-            intent.putExtra(WoVPreferences.OPPONENT_TYPE, WoVPreferences.OPPONENT_LOCAL_PLAYER);
-            startActivity(intent);
-        }
+        Intent intent = new Intent(MenuActivity.this, GameActivity.class);
+        intent.putExtra(WoVPreferences.OPPONENT_TYPE, WoVPreferences.OPPONENT_BOT);
+        startActivity(intent);
     }
 
     public void playAgainstLocalPlayer(View view) {
-        if (DBOpenHelper.getInstance(this).hasActiveGame()) {
-            new RestoreGameDialog(new PlayAgainstLocalPlayer()).execute();
-            return;
-        }
-        new PlayAgainstLocalPlayer().run();
+        Intent intent = new Intent(MenuActivity.this, GameActivity.class);
+        intent.putExtra(WoVPreferences.OPPONENT_TYPE, WoVPreferences.OPPONENT_LOCAL_PLAYER);
+        startActivity(intent);
     }
 
     public void viewGameHistory(View view) {
@@ -190,44 +203,49 @@ public class MenuActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private class GameLoadedFromServerReceiver extends BroadcastReceiver {
+    private class GcmMessagesReceiver extends BroadcastReceiver {
+        private boolean waitForGame = false;
+
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.d("GameActivity", "networkLoadGame broadcast recieved!");
-            Bundle tmp = intent.getBundleExtra(WoVPreferences.GAME_BUNDLE);
+            Bundle tmp = intent.getBundleExtra(WoVPreferences.BUNDLE);
             String data = tmp.getString(WoVProtocol.DATA);
-            intent = new Intent(MenuActivity.this, GameActivity.class);
-            intent.putExtra(WoVPreferences.OPPONENT_TYPE, WoVPreferences.OPPONENT_NETWORK_PLAYER);
-            intent.putExtra(WoVPreferences.GAME_JSON_DATA, data);
-            unregisterReceiver(gameLoadedFromServerReceiver);
-            gameLoadedFromServerReceiver = null;
-            startActivity(intent);
+            String action = tmp.getString(WoVProtocol.ACTION);
+            switch (action) {
+                case WoVProtocol.GAME_LOADED:
+                    if (!waitForGame) {
+                        break;
+                    }
+                    Log.d("GameActivity", "networkLoadGame broadcast recieved!");
+                    intent = new Intent(MenuActivity.this, GameActivity.class);
+                    intent.putExtra(WoVPreferences.OPPONENT_TYPE, WoVPreferences.OPPONENT_NETWORK_PLAYER);
+                    intent.putExtra(WoVPreferences.GAME_JSON_DATA, data);
+                    waitForGame = false;
+                    startActivity(intent);
+                    break;
+            }
         }
     }
 
-    private class PlayOnline implements Runnable {
-        @Override
-        public void run() {
-            GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(MenuActivity.this);
-            Bundle data = new Bundle();
-            data.putString(WoVProtocol.ACTION, WoVProtocol.ACTION_USER_READY);
-            String id = UUID.randomUUID().toString();
-            try {
-                gcm.send(MenuActivity.this.getString(R.string.gcm_defaultSenderId) + "@gcm.googleapis.com", id, data);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            gameLoadedFromServerReceiver = new GameLoadedFromServerReceiver();
-            registerReceiver(gameLoadedFromServerReceiver, new IntentFilter(WoVPreferences.GAME_LOADED_FROM_SERVER_BROADCAST));
-        }
+    private void logIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(apiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void logOut() {
+        Auth.GoogleSignInApi.signOut(apiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        SharedPreferences preferences = getSharedPreferences(WoVPreferences.PREFERENCES, MODE_PRIVATE);
+                        preferences.edit().putLong(WoVPreferences.CURRENT_USER_ID, HumanPlayer.USER_ANONYMOUS.getId()).apply();
+                        updateUI();
+                    }
+                });
     }
 
     public void playOnline(View view) {
-        if (DBOpenHelper.getInstance(this).hasActiveGame()) {
-            new RestoreGameDialog(new PlayOnline()).execute();
-            return;
-        }
-        new PlayOnline().run();
+        WoVGcmListenerService.sendGcmMessage(MenuActivity.this, WoVProtocol.ACTION_USER_READY, new JsonObject());
     }
 
     public void clearDB() {
@@ -246,12 +264,11 @@ public class MenuActivity extends AppCompatActivity {
         boolean hasGame = DBOpenHelper.getInstance(this).hasActiveGame();
         long userId = getSharedPreferences(WoVPreferences.PREFERENCES, MODE_PRIVATE).getLong(WoVPreferences.CURRENT_USER_ID, -1);
         User user = DBOpenHelper.getInstance(this).getUserById(userId);
-        boolean isOnline = user.getId() != WoVPreferences.ANONYMOUS_NICKNAME_ID;
+        boolean isOnline = user.getId() != HumanPlayer.USER_ANONYMOUS.getId();
 
-        playOnlineButton.setEnabled(!hasGame);
         playAgainstBotButton.setEnabled(!hasGame);
         playAgainstLocalPlayerButton.setEnabled(!hasGame);
-        playOnlineButton.setEnabled(!hasGame /* FIXME && isOnline */);
+        playOnlineButton.setEnabled(!hasGame && isOnline);
         restoreGameButton.setEnabled(hasGame);
 
         navigationView.getMenu().findItem(R.id.drawer_login).setEnabled(!isOnline);
@@ -266,7 +283,40 @@ public class MenuActivity extends AppCompatActivity {
         figureSet.setHueZero(user.getColorZero());
         crossButton.setFigure(figureSet, BoardCellState.get(GameLogic.CellType.CROSS));
         zeroButton.setFigure(figureSet, BoardCellState.get(GameLogic.CellType.ZERO));
+    }
 
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("MainActivity", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            String token = acct.getIdToken();
 
+            JsonObject data = new JsonObject();
+            data.addProperty(WoVProtocol.GOOGLE_TOKEN, token);
+            WoVGcmListenerService.sendGcmMessage(this, WoVProtocol.ACTION_LOGIN, data);
+            showProgressDialog(getString(R.string.menu_connecting_to_server));
+            //Actual UI changes will come on message result
+        } else {
+            WoVGcmListenerService.sendGcmMessage(this, WoVProtocol.ACTION_LOGOUT, null);
+            updateUI();
+        }
+    }
+
+    private void showProgressDialog(String message) {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage(message);
+            progressDialog.setIndeterminate(true);
+            /* FIXME progressDialog.setCancelable(false); */
+        }
+
+        progressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.hide();
+        }
     }
 }
--- app/src/main/java/net/ldvsoft/warofviruses/WoVGcmListenerService.java
+++ app/src/main/java/net/ldvsoft/warofviruses/WoVGcmListenerService.java
@@ -1,10 +1,16 @@
 package net.ldvsoft.warofviruses;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class WoVGcmListenerService extends GcmListenerService {
 
@@ -23,20 +29,33 @@ public class WoVGcmListenerService extends GcmListenerService {
         Log.i(TAG, "Message: " + data.toString());
         String action = (String) data.get(WoVProtocol.ACTION);
         Intent intent = new Intent();
-        if (action.equals(WoVProtocol.ACTION_TURN)) {
-            intent.putExtra(WoVPreferences.TURN_BUNDLE, data);
-            intent.setAction(WoVPreferences.TURN_BROADCAST);
-            sendBroadcast(intent);
-        } else if (action.equals(WoVProtocol.GAME_LOADED)) {
-            intent.putExtra(WoVPreferences.GAME_BUNDLE, data);
-            intent.setAction(WoVPreferences.GAME_LOADED_FROM_SERVER_BROADCAST);
-            sendBroadcast(intent);
+        intent.putExtra(WoVPreferences.BUNDLE, data);
+        switch (action) {
+            case WoVProtocol.ACTION_TURN:
+                intent.setAction(WoVPreferences.TURN_BROADCAST);
+                break;
+            case WoVProtocol.GAME_LOADED:
+                intent.setAction(WoVPreferences.MAIN_BROADCAST);
+                break;
         }
-        /**
-         * Production applications would usually process the message here.
-         * Eg: - Syncing with server.
-         *     - Store message in local database.
-         *     - Update UI.
-         */
+        sendBroadcast(intent);
     }
+
+    public static void sendGcmMessage(Context context, String action, JsonObject data) {
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
+        String id = "m-" + UUID.randomUUID().toString();
+        Bundle message = new Bundle();
+        message.putString(WoVProtocol.ACTION, action);
+        if (data != null) {
+            message.putString(WoVProtocol.DATA, data.toString());
+        }
+        try {
+            Log.d(TAG, "SENDING " + action + " TO " + context.getString(R.string.gcm_defaultSenderId) + " ID " + id);
+            gcm.send(context.getString(R.string.gcm_defaultSenderId) + "@gcm.googleapis.com", id, (long) 0, message);
+            Log.d(TAG, "OVER?");
+        } catch (IOException e) {
+            Log.wtf(TAG, "Something really wrong:", e);
+        }
+    }
+
 }
